d06fb94bedd88b945e98cbd14b12a20ec47c77dd
Tenetor3 <juaned@gmail.com>
2017-10-08 00:31:27 -0300

    Initial Commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Reddit Clone</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Reddit Clone</h1>
+        </div>
+    </div>
+</div>
+<br>
+
+    @yield("content")
+</body>
+</html>
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Title</h2>
+            <p>Posted 5 hours ago</p>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Title</h2>
+            <p>Posted 5 hours ago</p>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Title</h2>
+            <p>Posted 5 hours ago</p>
+        </div>
+    </div>
+
+@endsection
